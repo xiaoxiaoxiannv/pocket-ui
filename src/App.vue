89f6b213e2498757fafc0c64b09bb88c48888cc9
Luna <1017929760@qq.com>
2020-11-20 19:30:57 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Luna/>
+</template>
+
+<script>
+import Luna from './components/Luna.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Luna
+  }
+}
+</script>
