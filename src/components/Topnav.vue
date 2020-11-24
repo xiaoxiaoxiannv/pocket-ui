bea8777d880d12b55d5b891fbde5776f88c1e51b
Luna <1017929760@qq.com>
2020-11-24 13:01:55 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="topnav">
-        <div class="logo" @click="toggleMenu">LOGO</div>
+        <div class="logo">LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
-        <span class="toggleAside"></span>
+        <span class="toggleAside"  @click="toggleMenu"></span>
     </div>
 </template>
 <style lang="scss" scoped>
@@ -32,8 +32,9 @@
                 margin: 0 1em;
             }
         }
-        > .toggleAside{
-            display: inline-block;
+
+        > .toggleAside {
+            display: none;
             width: 24px;
             height: 24px;
             background: red;
@@ -42,9 +43,17 @@
             top: 50%;
             transform: translateY(-50%);
         }
+
         @media (max-width: 500px) {
-            > .menu{display: none}
-            > .logo{margin: 0 auto}
+            > .menu {
+                display: none
+            }
+            > .logo {
+                margin: 0 auto
+            }
+            > .toggleAside {
+                display: inline-block;
+            }
         }
     }
 </style>
