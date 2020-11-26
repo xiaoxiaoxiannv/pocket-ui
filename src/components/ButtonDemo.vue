ef55c64775881941327bb5b9b9329fa16b5d0fb6
Luna <1017929760@qq.com>
2020-11-26 17:27:57 +0800

    支持size属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,8 +6,14 @@
     <div>
         <Button>你好</Button>
         <Button theme="button">你好</Button>
-        <Button theme="link">你好</Button>
-        <Button theme="text">你好</Button>
+        <Button theme="link">链接按钮</Button>
+        <Button theme="text">文本按钮</Button>
+    </div>
+    <h1>示例2</h1>
+    <div>
+        <Button size="big">大</Button>
+        <Button>正常大小</Button>
+        <Button size="small">小</Button>
     </div>
 </template>
 <script lang="ts">
