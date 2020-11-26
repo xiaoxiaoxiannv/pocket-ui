f123c3b7b51ec1500c2463556f987a3c1e5c7a87
Luna <1017929760@qq.com>
2020-11-26 13:21:31 +0800

    优化代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -7,13 +7,13 @@
   import {ref} from 'vue';
 
   export default {
-    props:{
+    props: {
       value: Boolean
     },
-    setup(props,context) {
-      const toggle = ()=>{
-        context.emit('update:value',!props.value)
-      }
+    setup(props, context) {
+      const toggle = () => {
+        context.emit('update:value', !props.value);
+      };
       return {toggle};
     }
   };
@@ -28,33 +28,41 @@
         background: #bfbfbf;
         border-radius: $h/2;
         position: relative;
-    }
 
-    span {
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        height: $h2;
-        width: $h2;
-        background: white;
-        border-radius: $h2/2;
-        transition: all 250ms;
-    }
-    button.checked{
-        background: #1890ff;
-    }
-
-    button.checked > span {
-        left: calc(100% - #{$h2} - 2px);
-    }
-    button:focus{
-        outline: none;
-    }
-    button:active{
-        > span{width: $h2 + 4px}
-    }
-    button.checked:active{
         > span {
-            width: $h2 + 4px; margin-left: -4px}
+            position: absolute;
+            top: 2px;
+            left: 2px;
+            height: $h2;
+            width: $h2;
+            background: white;
+            border-radius: $h2/2;
+            transition: all 250ms;
+        }
+
+        &.checked {
+            background: #1890ff;
+
+            > span {
+                left: calc(100% - #{$h2} - 2px);
+            }
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &:active {
+            > span {
+                width: $h2 + 4px
+            }
+        }
+
+        &.checked:active {
+            > span {
+                width: $h2 + 4px;
+                margin-left: -4px
+            }
+        }
     }
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -91,7 +91,8 @@
             }
         }
     }
-    main{
+
+    main {
         overflow: auto;
     }
 </style>
