c3dbca5ec2ae67503cc051351bc157f2cbef839a
Luna <1017929760@qq.com>
2020-11-26 17:38:26 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,8 +6,8 @@
     <div>
         <Button>你好</Button>
         <Button theme="button">你好</Button>
-        <Button theme="link">链接按钮</Button>
-        <Button theme="text">文本按钮</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
     </div>
     <h1>示例2</h1>
     <div>
@@ -24,6 +24,24 @@
         <Button size="big" theme="text">大</Button>
         <Button theme="text">正常大小</Button>
         <Button size="small" theme="text">小</Button>
+    </div>
+    <h1>示例3</h1>
+    <div>
+        <dir>
+            <Button level="main">主要按钮</Button>
+            <Button>普通按钮</Button>
+            <Button level="danger">危险按钮</Button>
+        </dir>
+        <dir>
+            <Button theme="link" level="main">主要链接按钮</Button>
+            <Button theme="link">普通链接按钮</Button>
+            <Button theme="link" level="danger">危险链接按钮</Button>
+        </dir>
+        <dir>
+            <Button theme="text" level="main">主要文字按钮</Button>
+            <Button theme="text">普通文字按钮</Button>
+            <Button theme="text" level="danger">危险文字按钮</Button>
+        </dir>
     </div>
 </template>
 <script lang="ts">
