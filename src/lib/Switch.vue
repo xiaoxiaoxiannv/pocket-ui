efaccd9b36f3af1801fb7bacf85f2b9309dcf803
Luna <1017929760@qq.com>
2020-11-26 13:10:35 +0800

    Switch css更新

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -25,7 +25,7 @@
         height: $h;
         width: $h*2;
         border: none;
-        background: grey;
+        background: #bfbfbf;
         border-radius: $h/2;
         position: relative;
     }
@@ -38,10 +38,10 @@
         width: $h2;
         background: white;
         border-radius: $h2/2;
-        transition: left 250ms;
+        transition: all 250ms;
     }
     button.checked{
-        background: blue;
+        background: #1890ff;
     }
 
     button.checked > span {
@@ -49,5 +49,12 @@
     }
     button:focus{
         outline: none;
+    }
+    button:active{
+        > span{width: $h2 + 4px}
+    }
+    button.checked:active{
+        > span {
+            width: $h2 + 4px; margin-left: -4px}
     }
 </style>
