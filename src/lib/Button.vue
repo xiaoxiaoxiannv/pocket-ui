d972cb6ea78339122e41bf603b02949c1e6a9e2b
Luna <1017929760@qq.com>
2020-11-26 17:45:58 +0800

    支持disabled属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="gulu-button" :class="classes">
+    <button class="gulu-button" :class="classes" :disabled="disabled">
         <slot/>
     </button>
 </template>
@@ -19,7 +19,11 @@
       level: {
         type: String,
         default: 'normal'
-      }
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
     },
     setup(props) {
       const {theme, size,level} = props;
@@ -41,6 +45,7 @@
     $blue: #40a9ff;
     $radius: 4px;
     $red: red;
+    $grey: grey;
     .gulu-button {
         box-sizing: border-box;
         height: $h;
@@ -150,6 +155,21 @@
                 &:focus {
                     color: darken($red, 10%);
                 }
+            }
+        }
+        &.gulu-theme-button {
+            &[disabled] {
+                cursor: not-allowed;
+                color: $grey;
+                &:hover {
+                    border-color: $grey;
+                }
+            }
+        }
+        &.gulu-theme-link, &.gulu-theme-text {
+            &[disabled] {
+                cursor: not-allowed;
+                color: $grey;
             }
         }
     }
