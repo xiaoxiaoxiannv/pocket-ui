9fe827b005052690da1973099a5a5651b2a7cd6f
Luna <1017929760@qq.com>
2020-11-26 15:32:32 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,11 +5,21 @@
     <h1>示例1</h1>
     <div>
         <Button>你好</Button>
+        <Button theme="button">你好</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
     </div>
 </template>
 <script lang="ts">
-    import Button from '../lib/Button.vue'
-    export default {
-        components: {Button}
+  import Button from '../lib/Button.vue';
+
+  export default {
+    components: {Button},
+    setup() {
+      const onClick = () => {
+        console.log('hi');
+      };
+      return {onClick};
     }
+  };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,20 @@
 <template>
-    <button>
+    <button class="gulu-button"
+            :class="`theme-${theme}`"
+    >
         <slot/>
     </button>
 </template>
+<script lang="ts">
+  export default {
+    props: {
+      theme: {
+        type: String,
+        default: 'button'
+      }
+    }
+  };
+</script>
+<style lang="scss" scoped>
+
+</style>
