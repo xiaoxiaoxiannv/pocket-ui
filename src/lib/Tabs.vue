aa5a0d80743ef3a1e64fc00e1525c4077b465df7
Luna <1017929760@qq.com>
2020-12-01 11:05:45 +0800

    Tabs css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-        <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+    <div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <div class="gulu-tabs-content">
+            <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -15,10 +19,36 @@
           throw new Error('Tabs子标签必须是Tab');
         }
       });
-      const titles = defaults.map((tag)=>{
-        return tag.props.title
-      })
-      return {defaults,titles};
+      const titles = defaults.map((tag) => {
+        return tag.props.title;
+      });
+      return {defaults, titles};
     }
   };
 </script>
+<style lang="scss">
+    $blue: #40a9ff;
+    $color: #333;
+    $border-color: #d9d9d9;
+    .gulu-tabs {
+        &-nav {
+            display: flex;
+            color: $color;
+            border-bottom: 1px solid $border-color;
+            &-item {
+                padding: 8px 0;
+                margin: 0 16px;
+                cursor: pointer;
+                &:first-child {
+                    margin-left: 0;
+                }
+                &.selected {
+                    color: $blue;
+                }
+            }
+        }
+        &-content {
+            padding: 8px 0;
+        }
+    }
+</style>
