701be1c6f9f9ef1955ba631bf18e932bba461fcd
Luna <1017929760@qq.com>
2020-12-01 16:00:32 +0800

    切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,26 @@
 <template>
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav">
-            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)"
+                 :class="{selected: t===selected}" :key="index">{{t}}
+            </div>
         </div>
         <div class="gulu-tabs-content">
-            <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+            <component class="gulu-tabs-content-item"
+                       :class="{selected: c.props.title === selected}" v-for="(c,index) in defaults" :is="c"/>
         </div>
     </div>
 </template>
 <script lang="ts">
   import Tab from './Tab.vue';
+  import {computed} from 'vue'
 
   export default {
+    props: {
+      selected: {
+        type: String
+      }
+    },
     setup(props, context) {
       const defaults = context.slots.default();
       defaults.forEach((tag) => {
@@ -19,10 +28,18 @@
           throw new Error('Tabs子标签必须是Tab');
         }
       });
+      const current = computed(()=>{
+        return defaults.filter((tag)=>{
+          return tag.props.title === props.selected
+        })[0]
+      })
       const titles = defaults.map((tag) => {
         return tag.props.title;
       });
-      return {defaults, titles};
+      const select = (title:string)=>{
+        context.emit('update:selected',title)
+      }
+      return {defaults, titles,current,select};
     }
   };
 </script>
@@ -35,20 +52,32 @@
             display: flex;
             color: $color;
             border-bottom: 1px solid $border-color;
+
             &-item {
                 padding: 8px 0;
                 margin: 0 16px;
                 cursor: pointer;
+
                 &:first-child {
                     margin-left: 0;
                 }
+
                 &.selected {
                     color: $blue;
                 }
             }
         }
+
         &-content {
             padding: 8px 0;
+
+            &-item {
+                display: none;
+
+                &.selected {
+                    display: block;
+                }
+            }
         }
     }
 </style>
