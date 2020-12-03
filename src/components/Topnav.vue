2355792c28b8ada38089342892179175b898ec86
Luna <1017929760@qq.com>
2020-12-03 22:30:38 +0800

    从iconfont引入图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="topnav">
-        <div class="logo">LOGO</div>
+        <div class="logo">
+            <svg class="icon">
+                <use xlink:href="#icon-treasure"></use>
+            </svg>
+        </div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
@@ -23,6 +27,10 @@
         > .logo {
             max-width: 6em;
             margin-right: auto;
+            >svg {
+                width: 32px;
+                height: 32px;
+            }
         }
 
         > .menu {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,28 @@
 <template>
-    <div class="topnavAndBanner">
-        <Topnav/>
-        <div>
-            <div class="banner">
-                <h1>轱辘UI</h1>
-                <h2>一个UI框架</h2>
-                <p class="actions">
-                    <a href="https://github.com">GitHub</a>
-                    <router-link to="/doc">开始</router-link>
-                </p>
+    <div>
+        <div class="topnavAndBanner">
+            <Topnav/>
+            <div>
+                <div class="banner">
+                    <h1>Pocket UI</h1>
+                    <h2>一个超厉害的UI框架</h2>
+                    <p class="actions">
+                        <a href="https://github.com">GitHub</a>
+                        <router-link to="/doc">开始</router-link>
+                    </p>
+                </div>
             </div>
+        </div>
+        <div class="features">
+            <svg class="icon">
+                <use xlink:href="#icon-Vue"></use>
+            </svg>
+            <svg class="icon">
+                <use xlink:href="#icon-typescript"></use>
+            </svg>
+            <svg class="icon">
+                <use xlink:href="#icon-light"></use>
+            </svg>
         </div>
     </div>
 </template>
@@ -22,7 +35,14 @@
 </script>
 <style lang="scss" scoped>
     .topnavAndBanner {
-        background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+        background: white;
+    }
+
+    .features {
+        > svg {
+            width: 64px;
+            height: 64px;
+        }
     }
 
     .banner {
@@ -32,18 +52,31 @@
         align-items: center;
         flex-direction: column;
 
+        > h1 {
+            color: black;
+            font-family: "Goudy Old Style";
+        }
+
+        > h2 {
+            color: #73737a;
+            font-family: Consolas;
+            font-size: 16px;
+        }
+
         > .actions {
             padding: 8px 0;
 
             a {
                 margin: 0 8px;
-                background: #fff;
+                background: black;
+                color: white;
                 display: inline-block;
-                $h: 28px;
-                height: $h;
-                line-height: $h;
-                border-radius: $h/2;
-                padding: 0 8px;
+                padding: 8px 24px;
+                border-radius: 6px;
+
+                &:hover {
+                    text-decoration: none;
+                }
             }
         }
     }
