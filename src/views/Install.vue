5afd6e18b9734182687fb20a8ca8c88a5227c1b9
Luna <1017929760@qq.com>
2020-12-04 15:56:26 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        安装
-    </div>
+    <article>
+        <h1>安装</h1>
+        <p>打开终端运行下列命令：</p>
+        <pre><code>npm install pocket-ui</code></pre>
+        <p>或者</p>
+        <pre><code>yarn add pocket-ui</code></pre>
+
+        <p>
+            下一节：<a href="#/doc/get-start">开始使用</a>
+        </p>
+    </article>
 </template>
